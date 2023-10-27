86fbf4e9e3e4a768cb26feb4e0994a4efd08f121
Aleex-Deh <adehesadelgado@gmail.com>
2023-10-27 19:54:45 +0200

    Primera clase PYTHON Ayuda

--- python.py
+++ python.py
@@ -6,6 +6,7 @@ def metodo2():
     b = 1 + 3
     print(b)
 
+#Esto de abajo es un ejercicio diferente al de arriba
 def suma(numero1, numero2):
     return numero1 + numero2
 
@@ -26,10 +27,33 @@ if __name__ == "__main__":
     
     
     
-    nombres = ["Juanjo", "Pepito", "Salvador"]
+#Intento de arrays que se escriban abajo diciendo hola
+nombres = ["Juanjo", "Pepito", "Salvador"]
 
 for nombre in nombres:
     print(f"Hola, me llamo {nombre}")
 
     
+
     
+    
+    
+    
+    # Bucle infinito
+    
+    import time  # Importamos el módulo 'time' para poder usar la función 'sleep'.
+
+def bucle_infinito():  # Definimos una función llamada 'bucle_infinito'.
+    while True:  # Iniciamos un bucle 'while' con la condición siempre verdadera, lo que lo hace infinito.
+        print("I'm fucking slepping.")  # Imprime un mensaje en cada iteración del bucle.
+        time.sleep(5)  # Espera durante 5 segundos antes de la siguiente iteración del bucle.
+
+bucle_infinito()  # Llamamos a la función 'bucle_infinito' para comenzar el bucle infinito.
+
+
+
+
+ 
+
+ 
+ 
